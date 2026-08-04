3ecec5afc275b1cec1f7cfedc821ab9e80530779
gayatri64coder <gayatrikundalwal64@gmail.com>
2026-08-04 16:30:36 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (95.11%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,40 +1,38 @@
 class Solution {
-    public int myAtoi(String s) {
+    private int stringtointeger(String s , int sign , int i, long result){
+        //base case
+        if(i >= s.length()){
+            return (int) (result*sign);
+        }
+        char ch = s.charAt(i);
+        if(ch < '0' || ch > '9'){
+            return (int) (result*sign);
+        }
+        result = result * 10 +(ch -'0');
+        if (result > Integer.MAX_VALUE) {
+            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+        return stringtointeger(s,sign, i+1 , result);
         
-        s = s.trim(); // handeled white spaces 
-        long result =0;
-        int i = 0 ;
-        int sign = 1;
-        if( s.length() == 0){ // base case if string is empty
+    
+    }
+    public int myAtoi(String s) {
+        s = s.trim();//remove leading zeros
+        if(s.length() == 0){
             return 0;
         }
-
-        //signed integer
-        if(s.charAt(0) =='-' ){
+         
+        int sign = 1;
+        int i = 0;
+        long result =0 ;
+        if(s.charAt(0) == '-' ){
             sign = -1;
             i++;
-        }else if(s.charAt(0) =='+' ){
+        }else if (s.charAt(0) == '+'){
             i++;
         }
-
-        while(i < s.length()){
-            char ch = s.charAt(i);
-            //nonnumeric char handled
-            if(ch < '0' || ch > '9'  ){
-                break;
-            }
-            result = result * 10 + (ch -'0');
-            //integer overflow
-            if(sign * result > Integer.MAX_VALUE){
-                return Integer.MAX_VALUE;
-            }
-            if(sign * result < Integer.MIN_VALUE){
-                return Integer.MIN_VALUE;
-            }
-            i++;
-            
-        }
-        return (int)(sign * result);
+        result =  stringtointeger(s,sign ,i , result);
+        return (int) result;
     }
 }
 
